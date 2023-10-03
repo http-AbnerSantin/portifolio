df12b44a82d844618bc1a400ad74cce4a3e85f7d
http-AbnerSantin <abner.santin11@gmail.com>
2023-10-03 14:04:40 -0300

    03/10 fazendo o accordion list

--- js/cursos.js
+++ js/cursos.js
@@ -1,3 +1,17 @@
 export default function cursosDropDown() {
+    const accordionList = document.querySelectorAll('.cursos-accordion dt');
 
+    if(accordionList.length) {
+        accordionList[0].classList.add("activeAccordion");
+        accordionList[0].nextElementSibling.classList.add("activeAccordion");
+
+        function activeAccordion() {
+            this.classList.toggle("activeAccordion");
+            this.nextElementSibling.classList.toggle("activeAccordion");
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener("click", activeAccordion);
+        });
+    }
 }
